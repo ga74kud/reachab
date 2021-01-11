34166604cbf2430dfa1f968024c6ecac392f5419
Michael Hartmann <michael.hartmann@v2c2.at>
2021-01-11 16:28:02 +0100

    other reachability algorithm
    Signed-off-by: Michael Hartmann <michael.hartmann@v2c2.at>

--- source/reachability.py
+++ source/reachability.py
@@ -19,17 +19,18 @@ class reachability(object):
                         np.array([[0],
                          [0],
                          [1.6],
-                         [.7]
+                         [.7],
+                         [.3]
                         ]),
                         'g':
                         [
-                            np.array([[1], [0], [0], [0]]),
-                            np.array([[.4], [1.3], [0], [0]]),
-                            np.array([[.1], [0.0], [0.3], [0]]),
+                            np.array([[1], [0], [0], [0], [0]]),
+                            np.array([[.4], [1.3], [0], [0], [0]]),
+                            np.array([[.1], [0.0], [0.3], [0], [0]]),
                         ]}
-        self.params={'T': 2.0,
-                     'N': 4,
-                     'gamma': 2.0 #threshold for control input constraint (inf-norm)
+        self.params={'T': 0.4,
+                     'N': 10,
+                     'gamma': 0.01 #threshold for control input constraint (inf-norm)
                      }
         self.params['r']=self.params['T']/(self.params['N']+1)
         self.system_dynamics()
@@ -39,13 +40,13 @@ class reachability(object):
 
     def system_dynamics(self):
         self.A = np.matrix([[0, 0, 1, 0],
-                           [0, 0, 0, 1],
-                           [0, 0, 0, 0],
-                           [0, 0, 0, 0]])
-        self.B = np.matrix([[0, 0],
-                           [0, 0],
-                           [1, 0],
-                           [0, 1]])
+                            [0, 0, 0, 1],
+                            [0, 0, 0, 0],
+                            [0, 0, 0, 0]])
+        #self.B = np.matrix([[0, 0],
+        #                   [0, 0],
+        #                   [1, 0],
+        #                   [0, 1]])
         self.Phi=np.exp(self.params['r']*self.A)
     def multiplication_on_center(self, mat):
         return mat*np.matrix(self.zonotype['c'])
@@ -60,13 +61,13 @@ class reachability(object):
     def multiplication_on_zonotype(self, mat, zonotype):
         Z = {'c': None, 'g': None}
         Z['c']=mat*zonotype['c']
-        for i in range(0, np.size(zonotype['g'], 0)):
-            act_g=np.matrix(zonotype['g'][i,:])
+        for i in range(0, np.size(zonotype['g'], 1)):
+            act_g=np.matrix(zonotype['g'][:,i])
             if(i==0):
-                g=mat*np.transpose(act_g)
+                g=mat*act_g
             else:
-                g=np.hstack((g, mat*np.transpose(act_g)))
-        Z['g']=np.transpose(np.array(g))
+                g=np.hstack((g, mat*act_g))
+        Z['g']=g
         return Z
 
     def get_unique_vectors(self, vec):
@@ -92,8 +93,8 @@ class reachability(object):
     def minkowski_zonotypes(self, ZA, ZB):
         Z={'c': None, 'g': None}
         Z['c']=ZA['c']+ZB['c']
-        a=np.array(ZA['g'])
-        b=np.array(ZB['g'])
+        a=ZA['g']
+        b=ZB['g']
         new_g=np.vstack((a, b))
         Z['g'] = new_g
         return Z
@@ -101,19 +102,82 @@ class reachability(object):
         Z={'c': np.array([[0],
                          [0],
                          [0],
-                         [0]
+                         [0],
+                        [0]
                         ]),
            'g': None
            }
         b=[
-               np.array([[radius], [0], [0], [0]]),
-               np.array([[0], [radius], [0], [0]]),
-               np.array([[0], [0], [radius], [0]]),
-               np.array([[0], [0], [0], [radius]]),
+               np.array([[radius], [0], [0], [0], [0]]),
+               np.array([[0], [radius], [0], [0], [0]]),
+               np.array([[0], [0], [radius], [0], [0]]),
+               np.array([[0], [0], [0], [radius], [0]]),
+               np.array([[0], [0], [0], [0], [radius]]),
            ]
         Z['g']=self.get_unique_vectors(b)
         return Z
 
+    def approximate_reachable_set_v2(self):
+        all_R = []
+        '''
+        algorithm from: Girard, A.; "Efficient Computation of Reachable Sets of Linear Time-Invariant Systems with Inputs"
+        '''
+        # input
+        Omega_0 = {'c': np.matrix([[0],
+                            [0],
+                            [0],
+                            [0]
+                            ]),
+             'g': np.matrix([[1, 0],
+                             [0, 1],
+                             [0, 0],
+                             [0, 0]
+                            ])
+             }
+        U = {'c': np.matrix([[0],
+                             [0],
+                             [0],
+                             [0],
+                                   ]),
+                   'g': np.matrix([[.01, 0],
+                                   [0, .01],
+                                   [0, 0],
+                                   [0, 0]
+                                   ])
+                   }
+        #self.params['N']
+
+        # 1. step
+        X_0=Omega_0
+        X_i=X_0
+        # 2. step
+        V_0=U
+        V_i=V_0
+        # 3. step
+        S_0={'c': np.matrix([[0],
+                             [0],
+                             [0],
+                             [0],
+                                   ]),
+                   'g': np.matrix([[1, 0],
+                                   [0, 1],
+                                   [0, 0],
+                                   [0, 0]
+                                   ])
+                   }
+        S_i=S_0
+        # 4. step
+        for i in range(1, self.params['N'] - 1):
+            # 5. step
+            X_i = self.multiplication_on_zonotype(self.Phi, X_i)
+            # 6. step
+            S_i=self.minkowski_zonotypes(S_i, V_i)
+            # 7. step
+            V_i = self.multiplication_on_zonotype(self.Phi, V_i)
+            # 8. step
+            Omega_i=self.minkowski_zonotypes(S_i, V_i)
+            all_R.append(Omega_i)
+        return all_R
     def approximate_reachable_set(self):
         '''
         algorithm from: Girard, A.; "Reachability of Uncertain Linear Systems
@@ -155,23 +219,25 @@ class reachability(object):
         return all_R
 
     def sampling_trajectory(self, x0):
-        xk=[]
-        xk.append(x0)
+        xk=x0
         uk=np.matrix([[self.params['gamma']], [self.params['gamma']]])
         for i in range(0, self.params['N']):
-            xnew=self.params['r']*(self.A*xk[-1]+self.B*uk)+xk[-1]
-            xk.append(xnew)
-        return xk
+            xnew=self.params['r']*(self.A*xk[:, -1]+self.B*uk)+xk[:, -1]
+            xk=np.append(xk, xnew, axis=1)
+        traj=[(xk[0,i], xk[1,i]) for i in range(np.size(xk, 1))]
+        return traj
 
 if __name__ == '__main__':
     obj_reach = reachability()
     obj_visual = visualizer()
     obj_visual.show_point(obj_reach.zonotype['c'])
     zonoset_init=obj_reach.compute_zonoset(obj_reach.zonotype['c'], obj_reach.zonotype['g'])
-    R=obj_reach.approximate_reachable_set()
+    #R=obj_reach.approximate_reachable_set()
+    R=obj_reach.approximate_reachable_set_v2()
     obj_visual.filled_polygon(zonoset_init, 'lightsalmon')
     for act_zono in R:
         zonoset_P0 = obj_reach.compute_zonoset(act_zono['c'], act_zono['g'])
         obj_visual.filled_polygon(zonoset_P0, 'green')
-    obj_reach.sampling_trajectory(np.matrix([[2],[2],[2], [2]]))
+    #traj=obj_reach.sampling_trajectory(np.matrix([[2],[2],[2], [2], [2]]))
+    #obj_visual.show_traj(traj)
     obj_visual.show()
