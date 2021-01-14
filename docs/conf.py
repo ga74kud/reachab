3029cfe565899925ef4b6a62c84036989afeb58f
ga74kud <michaelhartmann10@googlemail.com>
2021-01-14 17:51:12 +0100

    Signed-off-by: ga74kud <michaelhartmann10@googlemail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -38,7 +38,8 @@ release = u'0.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ["recommonmark",
+              "sphinx_rtd_theme",]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
