0203363a8b36caf05a8dfcf8392d1f7b8b92b358
Michael Hartmann <michaelhartmann10@googlemail.com>
2021-03-29 12:23:13 +0200

    Signed-off-by: Michael Hartmann <michaelhartmann10@googlemail.com>

--- tests/optim_gekko_2.py
+++ tests/optim_gekko_2.py
@@ -9,8 +9,12 @@ class coordinate_system(object):
     def set_coord(self, coord):
         self.coord=coord
     def visualize(self, **kwargs):
-        ax1.arrow(self.coord[0,2], self.coord[1, 2], self.coord[0, 0], self.coord[1, 0], color=kwargs["color"])
-        ax1.arrow(self.coord[0,2], self.coord[1, 2], self.coord[0, 1], self.coord[1, 1], color=kwargs["color"])
+        ax1.arrow(self.coord[0,2], self.coord[1, 2], self.coord[0, 0], self.coord[1, 0], color=kwargs["color"], fc=kwargs["color"], ec="black", alpha=.6, width=.1,
+                  head_width=.3, head_length=.3)
+        ax1.arrow(self.coord[0,2], self.coord[1, 2], self.coord[0, 1], self.coord[1, 1], color=kwargs["color"], fc=kwargs["color"], ec="black", alpha=.6, width=.1,
+                  head_width=.3, head_length=.3)
+        circle1 = plt.Circle((self.coord[0,2], self.coord[1, 2]), 1, color=kwargs["color"], alpha=.2)
+        ax1.add_patch(circle1)
     def transform(self, mat):
         self.coord=np.dot(self.coord, mat)
     def get_information(self):
@@ -27,6 +31,11 @@ class control_over_manifold(object):
         self.control_input = {"u_0": self.m.Var(value=0, lb=-10, ub=10),
                       "u_1": self.m.Var(value=0, lb=-10, ub=10)}
         self.cost=self.m.Var(value=0)
+    def get_velocity(self):
+        vx = np.array(self.state["x1_2"])
+        vy = np.array(self.state["x1_3"])
+        v = np.sqrt(vx ** 2 + vy ** 2)
+        return v, vx, vy
     def set_initial_state(self, init_state):
         self.state= {"x1_0": self.m.Var(value=init_state[0]),
                      "x1_1": self.m.Var(value=init_state[1]),
@@ -55,7 +64,7 @@ transf_mat=np.array([[np.cos(angle), -np.sin(angle), 2], [np.sin(angle), np.cos(
 transf_mat_no_rot=np.array([[np.cos(angle), -np.sin(angle), 0], [np.sin(angle), np.cos(angle), 0], [0, 0, 1]])
 
 inv_transf_mat=np.linalg.inv(transf_mat)
-no_inv_transf_mat=np.linalg.inv(transf_mat_no_rot)
+no_rot_inv_transf_mat=np.linalg.inv(transf_mat_no_rot)
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
 coord_a=coordinate_system()
@@ -68,15 +77,13 @@ abc=control_over_manifold()
 abc.set_initial_state([0, 0, .3, -.5])
 abc.control([2, 0.3])
 rt=abc.get_information()
-vx=np.array(rt["state"]["x1_2"])
-vy=np.array(rt["state"]["x1_3"])
-v=np.sqrt(vx**2+vy**2)
+v,vx,vy=abc.get_velocity()
 time=rt["time"]
 cost=rt["cost"].value
 traj=np.vstack((rt["state"]["x1_0"].value, rt["state"]["x1_1"].value, np.ones((1, len(rt["state"]["x1_1"].value))), vx, vy, np.ones((1, len(rt["state"]["x1_1"].value)))))
 end_pos_new_coord=np.dot(inv_transf_mat, traj[0:3, -1])
 last_vel=traj[3:, -1]
-end_vel_new_coord=np.dot(no_inv_transf_mat, traj[3:, -1])
+end_vel_new_coord=np.dot(no_rot_inv_transf_mat, traj[3:, -1])
 new_state=np.vstack((end_pos_new_coord[0], end_pos_new_coord[1], end_vel_new_coord[0], end_vel_new_coord[1]))
 
 abc=control_over_manifold()
@@ -85,25 +92,30 @@ abc.control([.3,2.3])
 rt=abc.get_information()
 traj_b=np.vstack((rt["state"]["x1_0"].value, rt["state"]["x1_1"].value, np.ones((1, len(rt["state"]["x1_1"].value))), vx, vy, np.ones((1, len(rt["state"]["x1_1"].value)))))
 end_pos_new_coord=np.dot(transf_mat, traj_b[0:3, :])
-
-
+time_b=rt["time"]+time[-1]
+cost_b=rt["cost"].value
+vb,vx,vy=abc.get_velocity()
 #####################
 ### Visualization ###
 #####################
-ax1.plot(traj[0, :], traj[1, :],'-',label="position ref 1", color="blue")
-ax1.plot(end_pos_new_coord[0, :], end_pos_new_coord[1, :],'-',label="position ref 2", color="cyan")
+ax1.plot(traj[0, :], traj[1, :],'-',label="position ref 1", color="blue", linewidth=2.3)
+ax1.plot(end_pos_new_coord[0, :], end_pos_new_coord[1, :],'-',label="position ref 2", color="cyan", linewidth=2.3)
 ax1.legend(loc='best')
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
+ax1.grid()
 ax1.set(xlim=(-.3, 3.4), ylim=(-.3, 3.4))
 ax1.set_aspect('equal', adjustable='box')
 ax3 = ax2.twinx()
-ax2.plot(time, cost,'k-',label="control input", color="red", alpha=.7)
-ax3.plot(time, v,'k-',label="velocity", color="blue", alpha=.7)
+ax2.plot(time, cost,'k-',label="cost ref1", color="red", alpha=.7)
+ax3.plot(time, v,'k-',label="velocity ref1", color="blue", alpha=.7)
+ax2.plot(time_b, cost_b,'k-',label="cost ref2", color="orange", alpha=.7)
+ax3.plot(time_b, vb,'k-',label="velocity ref2", color="cyan", alpha=.7)
 ax2.legend(loc='upper left')
 ax3.legend(loc='lower right')
 ax2.set_xlabel('t')
 ax2.set_ylabel('u')
 ax3.set_ylabel('v')
+ax2.grid()
 plt.show()
 
